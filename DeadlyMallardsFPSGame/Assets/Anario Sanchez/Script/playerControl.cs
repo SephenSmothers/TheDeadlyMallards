341df30c09dfad62a162c0f80ac32509f8df7898
ENHam1009 <ENHam@student.fullsail.edu>
2023-07-20 09:40:58 -0400

    Small Change

--- DeadlyMallardsFPSGame/Assets/Anario Sanchez/Script/playerControl.cs
+++ DeadlyMallardsFPSGame/Assets/Anario Sanchez/Script/playerControl.cs
@@ -163,7 +163,7 @@ public class playerControl : MonoBehaviour, TakeDamage
 
             }
 
-            if (Physics.Raycast(bullet.position, bullet.forward, out hit, 1000f) && !hit.collider.CompareTag("Enemy"))
+            if (Physics.Raycast(bullet.position, bullet.forward, out hit, 1000f) && !hit.collider.CompareTag("Enemy") || !hit.collider.CompareTag("Player"))
             {
                 GameObject bulletHole = Instantiate(bulletHolePrefab, hit.point, Quaternion.Euler(0, 180, 0)) as GameObject;
                 bulletHole.transform.LookAt(hit.point + hit.normal);
