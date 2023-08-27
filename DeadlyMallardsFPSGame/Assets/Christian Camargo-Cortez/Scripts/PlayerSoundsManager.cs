123778d748e5f71abea76338db37e006b66d4aec
SephenSmothers <sephensmothers@yahoo.com>
2023-08-27 15:14:49 -0400

    Final Update

--- DeadlyMallardsFPSGame/Assets/Christian Camargo-Cortez/Scripts/PlayerSoundsManager.cs
+++ DeadlyMallardsFPSGame/Assets/Christian Camargo-Cortez/Scripts/PlayerSoundsManager.cs
@@ -115,6 +115,10 @@ public class PlayerSoundsManager : MonoBehaviour
         {
             PlayRifleShootingSound();
         }
+        else if (curGun.gunName == "GattlingGun")
+        {
+            PlayAssaultRifleShoot();
+        }
     }
 
     //REVOLVER SOUNDS
